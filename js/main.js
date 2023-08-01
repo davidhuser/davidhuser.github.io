b6c42ac36f1d8eb22cdec2c914d0223c26747139
David Huser <dhuser@baosystems.com>
2023-08-01 11:38:30 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,38 +1,51 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    feather.replace({ 'stroke-width': 1.75 });
+feather.replace({ 'stroke-width': 1.75 })
 
-    const toggler = document.getElementById('theme-toggler');
-    let currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const toggler = document.getElementById('theme-toggler');
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-    function updateIcon() {
-        while (toggler.firstChild) {
-            toggler.firstChild.remove();
-        }
-
-        let icon = document.body.classList.contains('dark-theme') ? 'sun' : 'moon';
-
-        let newIcon = feather.icons[icon].toSvg({ 'stroke-width': 1.25 });
-
-        toggler.insertAdjacentHTML('afterbegin', newIcon);
-        toggler.firstChild.setAttribute('aria-hidden', 'true');
+function updateIcon() {
+    while (toggler.firstChild) {
+        toggler.firstChild.remove();
     }
 
-    if (currentTheme) {
-        document.body.classList.add(currentTheme);
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    let icon = document.body.classList.contains('dark-theme') ? 'sun' : 'moon';
+    
+    let newIcon = feather.icons[icon].toSvg({ 'stroke-width': 1.25 });
+    
+    toggler.insertAdjacentHTML('afterbegin', newIcon);
+    toggler.firstChild.setAttribute('aria-hidden', 'true');
+}
+
+// Check for saved theme in localStorage
+let savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+    // If theme was saved, apply it
+    document.body.classList.add(savedTheme);
+} else {
+    // If no theme was saved, apply system preference
+    if (prefersDarkScheme.matches) {
         document.body.classList.add('dark-theme');
+    } else {
+        document.body.classList.add('light-theme');
     }
+}
+
+updateIcon();
+
+toggler.addEventListener('click', function() {
+    let currentTheme;
+    
+    if (document.body.classList.contains('dark-theme')) {
+        document.body.classList.replace('dark-theme', 'light-theme');
+        currentTheme = 'light-theme';
+    } else {
+        document.body.classList.replace('light-theme', 'dark-theme');
+        currentTheme = 'dark-theme';
+    }
+
+    // Save theme preference to localStorage
+    localStorage.setItem('theme', currentTheme);
 
     updateIcon();
-
-    toggler.addEventListener('click', function () {
-        if (document.body.classList.contains('dark-theme')) {
-            document.body.classList.remove('dark-theme');
-            localStorage.setItem('theme', 'light');
-        } else {
-            document.body.classList.add('dark-theme');
-            localStorage.setItem('theme', 'dark');
-        }
-        updateIcon();
-    });
 });
